d723d8be4a8551cbcde6c560bb98af3520106175
Tyler <tthatch93@gmail.com>
2022-11-26 18:42:35 -0700

    added email

--- app/contact_me/page.tsx
+++ app/contact_me/page.tsx
@@ -1,11 +1,54 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  EmailIcon,
+  EmailShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from "next-share"
 
 const ContactMe: React.FunctionComponent = (props) => {
+  const [name, setName] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+  const [message, setMessage] = useState<string>("")
+  const [submitted, setSubmitted] = useState<boolean>(false)
+
   const sendMail = () => {
     const mailto: string = "mailto:tthatch93@gmail.com?subject=Test subject&body=Body content"
     window.location.href = mailto
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log("Sending")
+    let data = {
+      name,
+      email,
+      message,
+    }
+
+    fetch("/api/contact.js", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Response received")
+      if (res.status === 200) {
+        console.log("Response succeeded!")
+        setSubmitted(true)
+        setName("")
+        setEmail("")
+        setMessage("")
+      }
+    })
   }
 
   return (
@@ -25,28 +68,70 @@ const ContactMe: React.FunctionComponent = (props) => {
               Just want an awesome art piece of your favorite anime character?
             </div>
             {/* CONTACT FORM */}
-            <div className="flex justify-between">
-              <button
-                onClick={sendMail}
-                type="submit"
-                className="px-5 py-3 text-4xl text-center text-black bg-white rounded-lg font-amatic sm:w-fit"
-              >
-                Send Email
-              </button>
-              <button
-                onClick={sendMail}
-                type="submit"
-                className="flex px-5 py-3 space-x-3 text-4xl text-black bg-white rounded-lg font-amatic sm:w-fit"
-              >
-                <div>Contact On</div>
-              </button>
-              <button
-                onClick={sendMail}
-                type="submit"
-                className="px-5 py-3 text-4xl text-center text-black bg-white rounded-lg font-amatic sm:w-fit"
-              >
-                Send Email
-              </button>
+            <div>
+              <form className="flex flex-col items-center justify-center w-full mb-10 space-y-4 font-amatic">
+                <div className="flex flex-col">
+                  <label className="text-xl text-left" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="h-10 text-xl w-[700px]"
+                    onChange={(e) => {
+                      setName(e.target.value)
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="email" className="text-xl text-left">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="h-10 text-xl w-[700px]"
+                    onChange={(e) => {
+                      setEmail(e.target.value)
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="message" className="text-xl text-left">
+                    Commission Details
+                  </label>
+                  <textarea
+                    name="message"
+                    className="text-xl h-36 w-[700px]"
+                    onChange={(e) => {
+                      setMessage(e.target.value)
+                    }}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  className="px-5 py-3 text-4xl text-center text-black bg-white rounded-lg font-amatic sm:w-fit"
+                  onClick={(e) => {
+                    handleSubmit(e)
+                  }}
+                />
+              </form>
+            </div>
+            <div>
+              <div className="pb-4 text-4xl font-bold text-center font-amatic">
+                Or contact me on social media!
+              </div>
+              <div className="flex items-center justify-center w-full space-x-4">
+                <FacebookShareButton url={"http://localhost:3000"}>
+                  <FacebookIcon size={80} round />
+                </FacebookShareButton>
+                <LinkedinShareButton url={"http://localhost:3000"}>
+                  <LinkedinIcon size={80} round />
+                </LinkedinShareButton>
+                <TwitterShareButton url={"http://localhost:3000"}>
+                  <TwitterIcon size={80} round />
+                </TwitterShareButton>
+              </div>
             </div>
           </div>
         </div>
